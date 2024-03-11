c36428cfa81aba998fe6597aba23148d8bd4121c
jiskiw <jon@tundrasupply.ca>
2024-03-10 19:26:53 -0600

    Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://ai.tundrasupply.cloud'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Tundra Supply AI</span>
       </a>
     </span>
   );
